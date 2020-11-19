a2bb203067deb57f68d2f4771aaafbe8a6789451
mmcloos <mmcloos@uno.edu>
2020-11-19 13:31:19 -0600

    module 7 assignment

---
+++ module7.py
@@ -0,0 +1,26 @@
+"""
+Maggie Cloos
+11-18-2020
+Agile UNO Module 7
+"""
+
+# 1
+# import my_module & pprint
+import my_module
+
+import pprint
+
+# 2
+# use the greeting method from my_module to print out your name
+my_module.greeting('Maggie')
+
+# 3
+# use the letter_text module to print out a string
+my_module.letter_text(name = "Maggie", amount = "10", denomination = "dollars")
+
+# 4
+# use the my_module.my_json_data and print it out
+
+# 5
+# import the my_json_data as my_data and print out the my_json_data using pprint
+
