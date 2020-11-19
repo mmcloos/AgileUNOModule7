debf561b58c23052b9ebed1e536709a2f3086735
mmcloos <mmcloos@uno.edu>
2020-11-19 14:14:54 -0600

    revised code

--- module7.py
+++ module7.py
@@ -12,7 +12,9 @@ import pprint
 
 # 2
 # use the greeting method from my_module to print out your name
-my_module.greeting('Maggie')
+name = "Maggie"
+my_module.greeting(name)
+
 
 # 3
 # use the letter_text module to print out a string
@@ -20,7 +22,10 @@ my_module.letter_text(name = "Maggie", amount = "10", denomination = "dollars")
 
 # 4
 # use the my_module.my_json_data and print it out
+my_module.my_json_data()
+print(my_json_data())
 
 # 5
 # import the my_json_data as my_data and print out the my_json_data using pprint
-
+my_data = my_json_data
+pprint(my_data)
